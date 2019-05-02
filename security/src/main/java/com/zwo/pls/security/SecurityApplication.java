0277f4a72b38905423c8d664cbe8a599d92e2a0e
huang <jixin.huang@artisantechnologies.cn>
2019-05-02 13:19:16 +0800

    修改数据库连接

--- security/src/main/java/com/zwo/pls/security/SecurityApplication.java
+++ security/src/main/java/com/zwo/pls/security/SecurityApplication.java
@@ -2,13 +2,20 @@ package com.zwo.pls.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @EnableTransactionManagement
 @SpringBootApplication
 @MapperScan(basePackages = {"com.zwo.pls.*.mapper"})
-public class SecurityApplication {
+public class SecurityApplication  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SecurityApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
