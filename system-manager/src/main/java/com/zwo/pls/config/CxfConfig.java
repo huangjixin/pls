c24d8548caed87830cd7b6e226779d12c1860acb
huang <jixin.huang@artisantechnologies.cn>
2019-07-24 17:58:32 +0800

    添加Web Service认证

--- system-manager/src/main/java/com/zwo/pls/config/CxfConfig.java
+++ system-manager/src/main/java/com/zwo/pls/config/CxfConfig.java
@@ -1,9 +1,7 @@
 package com.zwo.pls.config;
 
 import com.zwo.pls.modules.system.service.ITestWebService;
-import com.zwo.pls.modules.system.service.impl.TestWebServiceImpl;
 import org.apache.cxf.Bus;
-import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +23,9 @@ public class CxfConfig {
     public Endpoint endpoint() {
         EndpointImpl endpoint = new EndpointImpl(bus,testWebService);
         endpoint.publish("/testWebService");//接口发布在目录下
+
+        //通过拦截器校验用户名与密码
+        endpoint.getInInterceptors().add(new AuthInterceptor());
         return endpoint;
     }
 
