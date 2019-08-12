7454e675227dcd17850a1554974ba7aaf004e57c
huang <jixin.huang@artisantechnologies.cn>
2019-08-12 15:09:30 +0800

    修改startup.sh脚本

--- security/startup.sh
+++ security/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo 'maven 构建开始'
-mvn clean package -Dmaven.test.skip=true
-echo 'maven 构建结束'
+#echo 'maven 构建开始'
+#mvn clean package -Dmaven.test.skip=true
+#echo 'maven 构建结束'
 #注意if格式判断，线程筛选。
 security_pid=$(ps -ef | grep pls-security | grep -v grep | awk '{print $2}')
 if [ -n "$security_pid" ];then
 	kill -9 $security_pid
 fi
 #-Dhudson.util.ProcessTree.disable=true
-setsid java -jar target/pls-security.war --spring.profiles.active="dev" --hudson.util.ProcessTree.disable=true &
+nohup java -jar target/pls-security.war --spring.profiles.active="dev" &
