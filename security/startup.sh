9da92c24938b2a3432d7eda53bcc735db3ac22fb
huang <jixin.huang@artisantechnologies.cn>
2019-08-12 14:02:52 +0800

    新增startup.sh脚本文件

--- security/startup.sh
+++ security/startup.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+echo 'maven 构建开始'
+mvn clean package -Dmaven.test.skip=true
+echo 'maven 构建结束'
+#注意if格式判断，线程筛选。
+security_pid=$(ps -ef | grep pls-security | grep -v grep | awk '{print $2}')
+if [ -n "$security_pid" ];then
+	kill -9 $security_pid
+fi
+
 nohup java -jar target/pls-security.war -Dspring.profiles.active="dev" &
--- system-manager/startup.sh
+++ system-manager/startup.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+echo 'maven 构建开始'
+mvn clean package -Dmaven.test.skip=true
+echo 'maven 构建结束'
+#注意if格式判断，线程筛选。
+system_pid=$(ps -ef | grep pls-system-manager | grep -v grep | awk '{print $2}')
+if [ -n "$system_pid" ];then
+	kill -9 $system_pid
+fi
 nohup java -jar target/pls-system-manager.jar -Dspring.profiles.active="dev" &
