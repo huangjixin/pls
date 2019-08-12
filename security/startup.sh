75d939e55ded3c8d2755e80ad1d46ba4de090052
huang <jixin.huang@artisantechnologies.cn>
2019-08-12 17:25:03 +0800

    修改startup.sh文件

--- security/startup.sh
+++ security/startup.sh
@@ -1,10 +1,16 @@
-#echo 'maven 构建开始'
-#mvn clean package -Dmaven.test.skip=true
-#echo 'maven 构建结束'
+#!/bin/sh
+Tag="0.0.$BUILD_NUMBER" #BUILD_NUMBER为内置的构建次数
+#security/pom.xml
+#构建安全模块
+cd security
+echo 'maven 构建pls-security开始'
+mvn clean package -Dmaven.test.skip=true
+echo 'maven 构建pls-security结束'
+
 #注意if格式判断，线程筛选。
 security_pid=$(ps -ef | grep pls-security | grep -v grep | awk '{print $2}')
 if [ -n "$security_pid" ];then
 	kill -9 $security_pid
 fi
-#-Dhudson.util.ProcessTree.disable=true
-java -jar target/pls-security.war --spring.profiles.active="dev"
+#避免被Jenkins杀死进程。
+BUILD_ID=dontKillMe nohup java -jar target/pls-security.war --spring.profiles.active="dev" > pls-security-log.log &
--- system-manager/startup.sh
+++ system-manager/startup.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-echo 'maven 构建开始'
+#构建系统模块
+cd ../system-manager
+echo 'maven 构建pls-system-manager开始'
 mvn clean package -Dmaven.test.skip=true
-echo 'maven 构建结束'
-#注意if格式判断，线程筛选。
+echo 'maven 构建pls-system-manager结束'
 system_pid=$(ps -ef | grep pls-system-manager | grep -v grep | awk '{print $2}')
 if [ -n "$system_pid" ];then
 	kill -9 $system_pid
 fi
-#-Dhudson.util.ProcessTree.disable=true
-nohup java -jar target/pls-system-manager.jar --spring.profiles.active="dev" -Dhudson.util.ProcessTree.disable=true &
+#避免被Jenkins杀死进程。
+BUILD_ID=dontKillMe nohup java -jar target/pls-system-manager.jar --spring.profiles.active="dev" > pls-system-manager-log.log &
