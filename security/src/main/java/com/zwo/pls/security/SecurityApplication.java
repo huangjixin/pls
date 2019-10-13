f3d189bfb1effe7c4b0b6b6b715710a41fa37f91
huangjixin <517714860@qq.com>
2019-10-13 21:51:40 +0800

    修正配置文件

--- security/src/main/java/com/zwo/pls/security/SecurityApplication.java
+++ security/src/main/java/com/zwo/pls/security/SecurityApplication.java
@@ -3,7 +3,10 @@ package com.zwo.pls.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -15,6 +18,11 @@ public class SecurityApplication  extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SecurityApplication.class);
+    }
+
+    @Bean
+    ServletWebServerFactory servletWebServerFactory(){
+        return new TomcatServletWebServerFactory();
     }
 
     public static void main(String[] args) {
